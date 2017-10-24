1f7ae969f23662d28455b99d328359bf0a379311
Sebastian Pucilowski <smopucilowski@gmail.com>
2017-10-24 23:34:11 +1100

    bike: added BMC Teammachine 2018

--- bmc/__init__.py
+++ bmc/__init__.py
@@ -1,2 +1,4 @@
 from .roadmachine import _import
 roadmachine = _import()
+from .teammachine import _import
+teammachine = _import()
---
+++ bmc/teammachine/__init__.py
@@ -0,0 +1,21 @@
+from ..common import geometry_to_points, points_to_tubes, points_to_cockpit
+
+#______________________________________________________________________________
+def import_2018():
+    from .year_2018 import sizes, geometry
+    out = dict()
+    for index, size in enumerate(sizes):
+        geo       = {k:v[index] for k,v in geometry.items()}
+        points    = geometry_to_points(geo)
+        out[size] = {
+            'points':   geometry_to_points(geo),
+            'tubes':    points_to_tubes(points),
+            'cockpit':  points_to_cockpit(points),
+        }
+    return out
+
+#______________________________________________________________________________
+def _import():
+    return {
+        2018: import_2018(), # no change
+    }
