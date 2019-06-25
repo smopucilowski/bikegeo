ca46743aec622ae443a6a9f674c9801c363bb783
Ryan Harden <hardenrm@hydra.int.ancker.net>
2019-06-25 11:56:07 -0500

    fixed draw.py, spec init.py, allez sprint geo file

--- go.py
+++ go.py
@@ -1,26 +1,19 @@
 from draw import draw_bike, canvas
 import bmc
-import salsa
-import orbea
+import specialized
 
 with canvas():
     draw_bike(
-        bmc.roadmachine[2017][61],
+        bmc.roadmachine[2018][54],
         colour='black',
         wheels=True,
-        cockpit=(120,-6,25),
-        cranks=175,
+        cockpit=(90,-6,10),
+        cranks=165,
     )
     draw_bike(
-        salsa.fargo[2018]['lg'],
+        specialized.allez_sprint[2019][54],
         wheels=True,
-        cockpit=(80,6,25),
-        cranks=175,
+        cockpit=(90,-17,10),
+        cranks=165,
     )
 
-with canvas():
-    draw_bike(
-        orbea.katu[2018],
-        wheels=20*25.4,
-        cranks=170,
-    )
--- specialized/__init__.py
+++ specialized/__init__.py
@@ -1,2 +1,4 @@
 from .diverge_comp import _import
 diverge_comp = _import()
+from .allez_sprint import _import
+allez_sprint = _import()
